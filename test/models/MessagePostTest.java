087355e94f9829ebfd4efcd50c0e477cb2d7f71a
20090004 <leefitzgerald13@gmail.com>
2026-03-26 15:13:14 +0000

    Test added to MessagePostTest

--- test/models/MessagePostTest.java
+++ test/models/MessagePostTest.java
@@ -14,14 +14,16 @@ class MessagePostTest {
 
     @BeforeEach
     void setUp() {
-       messagePost1 = new MessagePost("Mary", "Hi there.");
-       messagePost2 = new MessagePost("John", "I'm on my way.");
+        messagePost1 = new MessagePost("Mary", "Hi there.");
+        messagePost2 = new MessagePost("John", "I'm on my way.");
+        messagePost3 = new MessagePost("Lee", "Hello World");
     }
 
     @AfterEach
     void tearDown() {
         messagePost1 = null;
         messagePost2 = null;
+        messagePost3 = null;
     }
 
     @Test
@@ -34,7 +36,7 @@ class MessagePostTest {
         assertEquals("Bye Bye", messagePost1.getMessage());
         assertEquals("Wait.. Wait..", messagePost2.getMessage());
         //TODO  test when try to set a message that's too long
-        messagePost3.setMessage("This message will be too long and won't work. I'm not sure how long this is but it's probably long enough now");
+        messagePost3.setMessage("This message will be too long and won't work. I'm not sure how long this is but it's probably long enough nowfffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         assertEquals("", messagePost3.getMessage());
     }
 }
